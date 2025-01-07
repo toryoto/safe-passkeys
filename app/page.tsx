0e00c587e86e4b55a872e9a7a9787e408fc19908
toryoto <ryoto@nerochain.io>
2025-01-08 02:54:22 +0900

    page

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,45 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
 
-export default function Home() {
+import type { Theme } from '@mui/material/styles'
+import { ThemeProvider } from '@mui/material/styles'
+import { PasskeyArgType } from '@safe-global/protocol-kit'
+import { useState } from 'react'
+
+import LoginWithPasskey from '@/components/LoginWithPasskey'
+import SafeAccountDetails from '@/components/SafeAccountDetails'
+import SafeThemeProvider from '../components/SafeThemeProvider'
+import { createPasskey, storePasskeyInLocalStorage } from '../lib/passkeys'
+
+function Create4337SafeAccount() {
+  const [selectedPasskey, setSelectedPasskey] = useState<PasskeyArgType>()
+
+  async function handleCreatePasskey() {
+    const passkey = await createPasskey()
+
+    storePasskeyInLocalStorage(passkey)
+    setSelectedPasskey(passkey)
+  }
+
+  async function handleSelectPasskey(passkey: PasskeyArgType) {
+    setSelectedPasskey(passkey)
+  }
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+    <SafeThemeProvider>
+      {(safeTheme: Theme) => (
+        <ThemeProvider theme={safeTheme}>
+          {selectedPasskey ? (
+            <SafeAccountDetails passkey={selectedPasskey} />
+          ) : (
+            <LoginWithPasskey
+              handleCreatePasskey={handleCreatePasskey}
+              handleSelectPasskey={handleSelectPasskey}
             />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+          )}
+        </ThemeProvider>
+      )}
+    </SafeThemeProvider>
+  )
 }
+
+export default Create4337SafeAccount
